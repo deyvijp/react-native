da0e416be6c925410b768d3abfcb7278ff76809a
Deyvi Pascual <dpascual@CECOMSACORP.LOCAL>
2025-12-05 16:26:02 -0400

    fix: remove nested git repo and add actual files

---
+++ AppRutas/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+});
+
+export default App;
